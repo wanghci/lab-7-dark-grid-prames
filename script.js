3de60761fcd3b2fb8b0432be909d2849e12fcb91
James Agresto <agrestjj@dukes.jmu.edu>
2024-02-29 15:34:01 -0500

    JS Update

--- script.js
+++ script.js
@@ -2,6 +2,19 @@ const os_df = document.getElementById('os-default');
 const dk_md = document.getElementById('dark-mode');
 const lt_md = document.getElementById('light-mode');
 
-// os_df.addEventListener("click", () => document.querySelector("body").className = "dark");
+const mql = window.matchMedia("(prefers-color-scheme: dark)");
+
+let defColSch;
+mql.addEventListener("change", (getColSch = getDefaultColorScheme))
+
+os_df.addEventListener("click", () => document.querySelector("body").className = getColSch);
 dk_md.addEventListener("click", () => document.querySelector("body").className = "dark");
 lt_md.addEventListener("click", () => document.querySelector("body").className = "");
+
+function getDefaultColorScheme(mql) {
+    if (mql.matches) {
+        document.querySelector("main").className = "dark"
+        return "dark"
+    }
+    return ""
+}
