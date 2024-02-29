ec27e7a30670c5505458d1df55c2c7d7376279bf
James Agresto <agrestjj@dukes.jmu.edu>
2024-02-29 15:46:46 -0500

    Final Product (I think)

--- script.js
+++ script.js
@@ -2,19 +2,21 @@ const os_df = document.getElementById('os-default');
 const dk_md = document.getElementById('dark-mode');
 const lt_md = document.getElementById('light-mode');
 
-const mql = window.matchMedia("(prefers-color-scheme: dark)");
+let defColSch = "";
+const mql = window.matchMedia('(prefers-color-scheme: dark)');
 
-let defColSch;
-mql.addEventListener("change", (getColSch = getDefaultColorScheme))
+mql.addEventListener('change', getDefaultColorScheme)
 
-os_df.addEventListener("click", () => document.querySelector("body").className = getColSch);
-dk_md.addEventListener("click", () => document.querySelector("body").className = "dark");
-lt_md.addEventListener("click", () => document.querySelector("body").className = "");
+os_df.addEventListener(
+    'click', () => document.querySelector('body').className = defColSch);
+dk_md.addEventListener(
+    'click', () => document.querySelector('body').classList.add('dark'));
+lt_md.addEventListener(
+    'click', () => document.querySelector('body').classList.remove('dark'));
 
 function getDefaultColorScheme(mql) {
-    if (mql.matches) {
-        document.querySelector("main").className = "dark"
-        return "dark"
-    }
-    return ""
+  if (mql.matches) {
+    document.querySelector('main').classList.add('dark');
+    defColSch = "dark";
+  }
 }
