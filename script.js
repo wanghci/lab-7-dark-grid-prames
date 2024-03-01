ea9259fe84a80af8a931a05ebc2276fc8141b425
2Poker <randpgamz@gmail.com>
2024-03-01 18:23:04 -0500

    finished

--- script.js
+++ script.js
@@ -2,10 +2,12 @@ const os_df = document.getElementById('os-default');
 const dk_md = document.getElementById('dark-mode');
 const lt_md = document.getElementById('light-mode');
 
-let defColSch = "";
-const mql = window.matchMedia('(prefers-color-scheme: dark)');
+var defColSch = "";
 
-mql.addEventListener('change', getDefaultColorScheme)
+if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
+  document.querySelector('body').classList.add('dark');
+  defColSch = "dark";
+}
 
 os_df.addEventListener(
     'click', () => document.querySelector('body').className = defColSch);
@@ -14,9 +16,5 @@ dk_md.addEventListener(
 lt_md.addEventListener(
     'click', () => document.querySelector('body').classList.remove('dark'));
 
-function getDefaultColorScheme(mql) {
-  if (mql.matches) {
-    document.querySelector('main').classList.add('dark');
-    defColSch = "dark";
-  }
-}
+console.log(document.querySelector('body').classList);
+
